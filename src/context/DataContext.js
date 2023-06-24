2fbba47ce22122257fa42834a408d37eeedc2c17
SahuPrachi <prachisahu998@gmail.com>
2023-06-24 22:13:35 +0530

    fix: minutes

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -18,13 +18,25 @@ export const DataProvider = ({ children }) => {
     return hours + "hours " + remainingMinutes + "minutes";
   };
 
+  const getOnlyMin = (time) => {
+    const currentTime = new Date();
+    const targetTime = new Date(time);
+    const timeDiffInMillis = Math.abs(
+      targetTime.getTime() - currentTime.getTime()
+    );
+    const minutes = Math.floor(timeDiffInMillis / (1000 * 60));
+
+    const remainingMinutes = minutes % 60;
+
+    return remainingMinutes;
+  };
+
   const reducerFunction = (state, action) => {
     switch (action.type) {
-      case "LATEST_SORT":
       case "SORT":
-        if (action.payload === state.sortBy) {
+        if (action.payload === "latest") {
           const newSortedData = [...state.sortedData].sort(
-            (a, b) => getTime(a.createdAt) - getTime(b.createdAt)
+            (a, b) => getOnlyMin(b.createdAt) - getOnlyMin(a.createdAt)
           );
           console.log(newSortedData);
           return {
@@ -32,11 +44,11 @@ export const DataProvider = ({ children }) => {
             sortedData: newSortedData,
             sortBy: action.payload,
           };
-        } else if (action.payload === state.sortBy) {
+        } else if (action.payload === "upvote") {
           const newSortedData = state.sortedData.sort(
             (a, b) => b.upvotes - b.downvotes - (a.upvotes - a.downvotes)
           );
-
+          console.log(newSortedData);
           return {
             ...state,
             sortedData: newSortedData,
